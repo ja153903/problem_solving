1397b7b681fa30081717468a772c6b15ec57644c
Jaime Abbariao <jaimeabbariao@gmail.com>
2022-01-12 22:23:48 -0500

    updating the problem statement

--- leetcode/monthly/january2022/minimum_number_of_arrows_to_burst_balloons.py
+++ leetcode/monthly/january2022/minimum_number_of_arrows_to_burst_balloons.py
@@ -10,7 +10,7 @@ The balloons are represented as a 2D integer array points where points[i] = [x_s
 denotes a balloon whose horizontal diameter stretches between x_start and x_end. You do not know the exact y-coordinates of the balloons.
 
 Arrows can be shot up directly vertically (in the positive y-direction) from different points along the x-axis.
-A balloon with xstart and xend is burst by an arrow shot at x if x_start <= x <= x_end. 
+A balloon with x_start and x_end is burst by an arrow shot at x if x_start <= x <= x_end. 
 There is no limit to the number of arrows that can be shot. A shot arrow keeps traveling up infinitely, bursting any balloons in its path.
 
 Given the array points, return the minimum number of arrows that must be shot to burst all balloons.
