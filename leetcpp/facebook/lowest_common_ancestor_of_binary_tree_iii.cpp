948e4aacea9dd87119063f04a3bfbfc44d202c0a
Jaime Abbariao <jaimeabbariao@gmail.com>
2022-01-17 01:16:50 -0500

    adding solution

--- leetcpp/facebook/lowest_common_ancestor_of_binary_tree_iii.cpp
+++ leetcpp/facebook/lowest_common_ancestor_of_binary_tree_iii.cpp
@@ -6,8 +6,27 @@ public:
   Node *parent;
 };
 
+/**
+ * Given two nodes of a binary tree p and q, return their lowest common ancestor (LCA).
+ *
+ * === Approach ===
+ * By definition, what is the lowest common ancestor? The lowest common ancestor is the
+ * node that both p and q share as its parent. Also note that the LCA can be itself.
+ *
+ * The approach here is to work as if this was the list intersection problem.
+ * If there is an intersection at any point, then we return that.
+ */
 class Solution {
 public:
   Node *lowestCommonAncestor(Node *p, Node *q) {
+    auto a = p;
+    auto b = q;
+
+    while (a != b) {
+      a = a == nullptr ? q : a->parent;
+      b = b == nullptr ? p : b->parent;
+    }
+
+    return a;
   }
 };
